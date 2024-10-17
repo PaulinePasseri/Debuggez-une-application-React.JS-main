6629fcf17ece403386f567a043e65f1c2ab7491f
Pauline Passeri <paulinecpasseri@gmail.com>
2024-10-17 18:49:11 +0200

    tests unitaires

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,9 +1,9 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import Home from './index'
+import Page from './index'
 
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
-    render(<Home />);
+    render(<Page />);
     await screen.findByText("Email");
     await screen.findByText("Nom");
     await screen.findByText("Prénom");
@@ -12,7 +12,7 @@ describe("When Form is created", () => {
 
   describe("and a click is triggered on the submit button", () => {
     it("the success message is displayed", async () => {
-      render(<Home />);
+      render(<Page />);
       fireEvent(
         await screen.findByText("Envoyer"),
         new MouseEvent("click", {
@@ -29,24 +29,28 @@ describe("When Form is created", () => {
 
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", async () => {
-    render(<Home/>)
-    const events = await screen.findAllByTestId("event-card")
-    expect(events.length).toBeGreaterThan(0)
-  })
-  it("a list of people is displayed", async () => {
-    render(<Home/>)
-    const people = await screen.findAllByTestId("person-card")
-    expect(people.length).toBeGreaterThan(0)
-  })
+  it("a list of events is displayed", () => {
+    render(<Page />);
+    const eventsTitle = screen.getByText("Nos réalisations");
+    expect(eventsTitle).toBeInTheDocument();
+  });
+
+  it("a list of people is displayed", () => {
+    render(<Page />);
+    const peopleTitle = screen.getByText("Notre équipe");
+    expect(peopleTitle).toBeInTheDocument();
+  });
+
   it("a footer is displayed", () => {
-    render(<Home/>)
-    const footer = screen.getByRole("contentinfo")
-    expect(footer).toBeInTheDocument();
-  })
+    render(<Page />);
+    const footerTitle = screen.getByText("Contactez-nous");
+    expect(footerTitle).toBeInTheDocument();
+  });
+
   it("an event card, with the last event, is displayed", async () => {
-    render(<Home/>)
-    const lastEvent = await screen.findByTestId("last-event-card")
-    expect(lastEvent).toBeInTheDocument()
-  })
-})
+    render(<Page />);
+    const lastEventTitle = await screen.findByText("Notre derniére prestation");
+    expect(lastEventTitle).toBeInTheDocument();
+  });
+
+});
